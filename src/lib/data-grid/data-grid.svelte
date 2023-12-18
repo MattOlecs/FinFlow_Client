ea06f15b454f2c17e073830f241b3794b84e86c2
Mateusz.Oleksik <mateusz.oleksik@vistex.com>
2023-12-18 09:00:52 +0100

    grid initialization

--- src/lib/data-grid/data-grid.svelte
+++ src/lib/data-grid/data-grid.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
 	import Grid from 'gridjs-svelte';
+	import { onMount } from 'svelte';
+
+	let grid: Grid;
 
 	const data = [
 		{ name: 'John', email: 'john@example.com' },
 		{ name: 'Mark', email: 'mark@gmail.com' }
 	];
+
+	onMount(() => {
+		let instance = grid.instance;
+		instance.forceRender();
+	})
+
 </script>
 
-<Grid {data} />
+<div>
+	<Grid bind:instance={grid} {data} />
+</div>
 
-<!-- <style>
-	/* @import 'data-grid.css';
-	@import 'https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css'; */
-</style> -->
+<style>
+	@import 'https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css';
+</style>
