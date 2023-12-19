5d91f4544f438299233011d394532a80cce70f59
Mateusz Oleksik <UL0240010@edu.uni.lodz.pl>
2023-12-19 20:07:38 +0100

    extract data grid to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,7 @@
 <script lang="ts">
 	import ChartsGrid, { addChart } from '$lib/grid/grid.svelte';
 	import { ChartType, type ChartDefinition } from '$lib/grid/grid.svelte';
-	import { onMount, onDestroy } from 'svelte';
-	import { GridApi, createGrid } from 'ag-grid-community';
-	import type { GridOptions } from 'ag-grid-community';
-	import '@ag-grid-community/styles/ag-grid.css';
-	import '@ag-grid-community/styles/ag-theme-alpine.css';
-	import '@ag-grid-community/styles/ag-theme-quartz.css';
-
-	let gridDiv: HTMLDivElement;
-	let gridApi: GridApi;
-
-	const columnDefs = [
-		{ headerName: 'Make', field: 'make' },
-		{ headerName: 'Model', field: 'model' },
-		{ headerName: 'Price', field: 'price' }
-	];
-
-	const rowData = [
-		{ make: 'Toyota', model: 'Celica', price: 35000 },
-		{ make: 'Ford', model: 'Mondeo', price: 32000 },
-		{ make: 'Porsche', model: 'Boxster', price: 72000 }
-	];
-
-	let gridOptions: GridOptions = {
-		columnDefs,
-		rowData
-		// Add other grid options as needed
-	};
-
-	onMount(() => {
-		gridApi = createGrid(gridDiv, gridOptions);
-	});
-
-	onDestroy(() => {
-		if (gridApi) {
-			gridApi.destroy();
-		}
-	});
+	import DataGrid from '$lib/data-grid/data-grid.svelte';
 
 	let gridComponent: ChartsGrid;
 	let data = [
@@ -62,8 +26,4 @@
 <button on:click|preventDefault={addDonutChart}>add donut</button>
 <button on:click|preventDefault={addBarChart}>add bar</button>
 
-<div
-	class="ag-theme-quartz-auto-dark"
-	bind:this={gridDiv}
-	style="height: 500px; width: 600px;"
-></div>
+<DataGrid />
