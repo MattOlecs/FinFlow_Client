b9d46b94e42e6fddc73c0706a258897c67f4cbf5
Mateusz Oleksik <ext-mateusz.oleksik@flowbird.group>
2024-06-16 21:49:09 +0200

    integrate with collm

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -2,9 +2,10 @@
 	import raportGenerator from '$lib/data/raportGenerator';
 
 	interface ISuggestionsResult {
-	id: number;
-	text: string;
-	similarity: number;
+		id: number;
+		text: string;
+		response: string,
+		similarity: number;
 	}
 
 	interface IAIResult {
@@ -16,10 +17,18 @@
 		text: string,
 		sender: string
 	}
+
+	interface ICreateChatResponse {
+		uId: string;
+		response: string;
+	}
 	
 	let suggestionsResult: ISuggestionsResult[] = [];
 	let message = "";
 	let messages: IMessage[] = [];
+	let chatUid = "";
+	let selectedSuggestionId = 0;
+	let suggestionValue = '';
 
 	let aiPrompt = "Jesteś doradcą finansowym w aplikacji. Będzie teraz prowadził rozmowę z użytkownikiem na podstawie jego raportu wydatków i przychodów za wskazany miesiąc. Nie możesz rozmawiać na inne tematy niż porady finansowe. Jako odpowiedź na to pytanie, oceń jego raport wydatków. Nie odnoś się do tej wstępnej instrukcji. Oto raport: "
 
@@ -28,21 +37,39 @@
 	let isInGptMode: boolean = false;
   
 	async function sendMessage() {
-	  	if (message.trim() === "") return;
+	  	if (!isInFirstAnswer && message.trim() === "") return;
   
 		if (isInFirstAnswer) {
-			let chatGptResponse = fetchGptResponse(aiPrompt + raportGenerator.getRaport())
-			messages = [...messages, { text: (await chatGptResponse).answer, sender: "api" }];
+			let chatResponse = await createConveration(raportGenerator.getRaport())
+			console.log('uiud:  ' + chatResponse!.uId)
+			chatUid = chatResponse!.uId;
+			messages = [...messages, { text: chatResponse!.response, sender: "api" }];
 			isInFirstAnswer = false;
 		}
 		else{
 			const userMessage = { text: message, sender: "user" };
 			messages = [...messages, userMessage];
 		
-			const apiResponse = await fetchSuggestions(message);
+			let response = await getConversationAnswer(message);
+			console.log('response:  ' + response)
+			messages = [...messages, { text: response, sender: "api" }];
 		}
 		
 		message = ""; // Reset input after sending
+	}
+
+	async function getSuggestions() {
+	  	if (!isInFirstAnswer && message.trim() === "") return;
+  
+		await fetchSuggestions(message);
+	}
+
+	async function selectSuggestion(id: number) {
+		if (selectedSuggestionId == id){
+			selectedSuggestionId = 0;
+			return;
+		}
+		selectedSuggestionId = id;
 	}
   
 
@@ -73,7 +100,7 @@
 		return aiResult;
 	}
 
-	async function fetchSuggestions(userMessage: string) {
+	async function fetchSuggestions(userMessage: string): Promise<ISuggestionsResult[] | undefined> {
 	  	try {
 			const response = await fetch('http://localhost:5295/Sentences/similar/suggestions', {
 				method: 'POST',
@@ -93,8 +120,79 @@
 			}
 		} catch (error) {
 			console.error("Error fetching API:", error);
-			return "Failed to get response.";
 	  }
+	  return undefined;
+	}
+
+	async function createConveration(userMessage: string): Promise<ICreateChatResponse | undefined>  {
+	  	try {
+			const response = await fetch('http://localhost:5295/Chat', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					context: aiPrompt,
+					firstMessage: userMessage
+				})
+			});
+
+			if (response.ok) {
+				return await response.json();
+			} else {
+				console.error('Failed to fetch data');
+			}
+		} catch (error) {
+			console.error("Error fetching API:", error);
+	  }
+	
+	  return undefined;
+	}
+
+	async function getConversationAnswer(userMessage: string): Promise<string> {
+	  	try { 
+			const response = await fetch('http://localhost:5295/Chat/' + chatUid, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					message: userMessage
+				})
+			});
+
+			if (response.ok) {
+				return await response.text();
+			} else {
+				console.error('Failed to fetch data');
+			}
+		} catch (error) {
+			console.error("Error fetching API:", error);
+	  }
+	
+	  return "";
+	}
+
+	async function getSuggestion(id: number): Promise<string> {
+		try { 
+			const response = await fetch('http://localhost:5295/Sentences/' + id, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+
+			if (response.ok) {
+				let result = await response.json();
+				return result.text;
+			} else {
+				console.error('Failed to fetch data');
+			}
+		} catch (error) {
+			console.error("Error fetching API:", error);
+	  }
+	
+	  return "";
 	}
 </script>
   
@@ -108,12 +206,28 @@
 	</div>
 	<div class="input-container">
 		<input
+			disabled={isInFirstAnswer}
 			type="text"
 			bind:value={message}
 			on:keydown={e => e.key === 'Enter' && sendMessage()}
 		/>
-		<button on:click={sendMessage}>Send</button>
+		<button on:click={sendMessage}>{isInFirstAnswer ? "Start conversation" : "Send"}</button>
+		<button on:click={getSuggestions}>Get suggestions</button>
 	</div>
+	<ul>
+		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		{#each suggestionsResult as item}
+		<li on:click={async () => await selectSuggestion(item.id)} style="cursor: pointer;">
+			&#x2022; {item.text}
+		</li>
+		{#if item.id == selectedSuggestionId}
+		<div class="detail">
+			{item.response}
+		</div>	
+		{/if}
+	  	{/each}
+	</ul>
 </main>
   
 <style>
@@ -158,6 +272,17 @@
 	}
 	button {
 		width: 10%;
+	}
+  	.detail {
+		margin-top: 8px;
+		background-color: #f0f0f0;
+		padding: 10px;
+		border-radius: 5px;
+		color: black;
+		transition: ease-in-out;
+  	}
+	li {
+		color: black;
 	}
 </style>
   
